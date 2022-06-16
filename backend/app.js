83310b6b1153a61c67c84d98a79771cd33fe3d08
Morgan Laversin <morganlaversin@gmail.com>
2022-06-16 11:15:46 +0200

    backend initialization

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+/*
+ * Configure database connection
+ */
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URI}`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('✔ Connected to database !'))
+  .catch(() => console.log('⚠ Database connection failure !'));
+
+/*
+ * Parses incoming JSON requests and puts the parsed data in req.body
+ */
+app.use(express.json());
+
+/*
+ * Allows cross-origin requests
+ */
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('test');
+});
+
+module.exports = app;
