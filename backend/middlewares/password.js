07fabbbd6a3ecec6ce8fcb8c9393c83f09b00b77
Morgan Laversin <morganlaversin@gmail.com>
2022-06-17 09:10:40 +0200

    improves password middleware

--- backend/middlewares/password.js
+++ backend/middlewares/password.js
@@ -3,20 +3,31 @@ const passwordValidator = require('password-validator');
 const schema = new passwordValidator();
 
 schema
-  .is().min(8)
-  .is().max(100)
-  .has().uppercase()
-  .has().lowercase()
-  .has().digits(1)
-  .has().not().spaces()
-  .is().not().oneOf(['Passw0rd', 'Password123', 'M0tdepasse', 'motdepass3', 'motd3passe']); // Blacklist these values
+  .is().min(8, 'Le mot de passe doit contenir 8 caractères minimum.')
+  .is().max(100, 'Le mot de passe doit contenir 100 caractères maximum.')
+  .has().uppercase(1, 'Le mot de passe doit contenir au moins une lettre majuscule.')
+  .has().lowercase(1, 'Le mot de passe doit contenir au moins une lettre minuscule.')
+  .has().digits(1, 'Le mot de passe doit contenir au moins un chiffre.')
+  .has().not().spaces(0, 'Les espaces ne sont pas autorisés')
+  .is().not().oneOf(
+    [
+      'Passw0rd',
+      'Pa55word',
+      'Pa55w0rd',
+      'Password123',
+      'M0tdepasse',
+      'Motdepass3',
+      'Motd3passe',
+      'M0tdepasse',
+    ],
+    'Mot de passe interdit.'
+  ); // Blacklist these values
 
 module.exports = (req, res, next) => {
   if (schema.validate(req.body.password)) {
     next();
   } else {
-    res.statusMessage =
-      'Le mot de passe doit contenir entre 8 et 100 caractères incluant au moins un chiffre et une lettre majuscule (les espaces ne sont pas autorisés)';
-    return res.status(400).send();
+    const error = schema.validate(req.body.password, { details: true })[0].message;
+    return res.status(400).json({ error });
   }
 };
