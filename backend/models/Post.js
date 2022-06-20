e804dcce12b09fa9f8a1473f86c6c2af94020676
Morgan Laversin <morganlaversin@gmail.com>
2022-06-20 11:36:50 +0200

    adds date fields in post schema

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -4,6 +4,8 @@ const postSchema = mongoose.Schema({
   userId: { type: String, required: true },
   message: { type: String, required: true },
   pictureUrl: { type: String, required: true },
+  createdAt: { type: String, required: true },
+  updatedAt: { type: String, required: true },
   likes: { type: Number, required: true, default: 0 },
   usersLiked: { type: [String], required: true, default: [] },
 });
