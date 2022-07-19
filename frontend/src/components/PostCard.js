6cc63e63cb47ec52c977aa0e45acc608c900b6f7
Morgan Laversin <morganlaversin@gmail.com>
2022-07-19 17:35:29 +0200

    add updatedAt field in posts display

--- frontend/src/components/PostCard.js
+++ frontend/src/components/PostCard.js
@@ -2,13 +2,17 @@ import Moment from 'moment';
 
 export default function PostCard({ post }) {
   const createdAt = Moment(post.createdAt).format('DD/MM/YY à hh:mm');
+  const updatedAt = Moment(post.updatedAt).format('DD/MM/YY à hh:mm');
 
   return (
     <article className='post-card'>
       <p className='post-card__username'>
         {post.user.firstname + ' ' + post.user.lastname}
       </p>
-      <p className='post-card__createdAt'>Posté le {createdAt} </p>
+      <p className='post-card__createdAt'>
+        Posté le {createdAt}{' '}
+        {createdAt !== updatedAt ? `- Modifié le ${updatedAt}` : ''}
+      </p>
       <p className='post-card__message'>{post.message}</p>
     </article>
   );
