62364ba6f984baf31e836fa1d02618f895c09324
Morgan Laversin <morganlaversin@gmail.com>
2022-07-20 12:22:58 +0200

    add a logout button in the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from '../assets/logo-gpm.png';
 
 export default function Header() {
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    window.location.href = 'http://localhost:4200/auth';
+  };
+
   return (
     <header className='header'>
       <div className='header-logo'>
         <img src={logo} alt='Groupomania' className='logo-gpm' />
       </div>
+      <button className='btn logout-btn' onClick={handleLogout}>
+        Logout
+      </button>
     </header>
   );
 }
