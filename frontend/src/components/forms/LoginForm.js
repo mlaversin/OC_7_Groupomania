e147b93a1aef6fa0caf562e0f52f453ef93a9ad8
Morgan Laversin <morganlaversin@gmail.com>
2022-08-11 22:47:15 +0200

    add password validation on the login form

--- frontend/src/components/forms/LoginForm.js
+++ frontend/src/components/forms/LoginForm.js
@@ -16,7 +16,20 @@ export default function LoginForm() {
     email: Yup.string()
       .email('Email invalide')
       .required("L'email est obligatoire"),
-    password: Yup.string().required('Le mot de passe est obligatoire'),
+    password: Yup.string()
+      .required('Le mot de passe est obligatoire')
+      .matches(
+        /^(?=.*[a-z])/,
+        'Le mot de passe doit contenir au moins une minuscule.'
+      )
+      .matches(
+        /^(?=.*[A-Z])/,
+        'Le mot de passe doit contenir au moins une majuscule.'
+      )
+      .matches(
+        /^(?=.*[0-9])/,
+        'Le mot de passe doit contenir au moins un chiffre.'
+      ),
   });
 
   const initialValues = {
