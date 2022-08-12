ab2b28533cec99532da4c80939581cd252fe1d73
Morgan Laversin <morganlaversin@gmail.com>
2022-08-12 07:36:58 +0200

    improves not found page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../components/layout/Layout';
 
 /*
  * This component is the not found page which is displayed
  * in the event of a 404 error
  */
 export default function NotFound() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
   return (
     <>
-      <main className='notfound-page'>
-        <h1>Erreur 404 - La page demandée n'existe pas !</h1>
-      </main>
+      <Layout>
+        <main className='notfound-page'>
+          <h1>Oups, la page demandée n'existe pas !</h1>
+          <button className='btn btn-primary' onClick={() => handleClick()}>
+            Retour à l'accueil
+          </button>
+        </main>
+      </Layout>
     </>
   );
 }
